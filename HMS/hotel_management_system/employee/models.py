dceb0cfdb8b8e74687f5db426e5bc795d65afffc
SurajM25 <surajmanjunath2@gmail.com>
2020-11-13 00:44:26 +0530

    creating employee model

--- HMS/hotel_management_system/employee/models.py
+++ HMS/hotel_management_system/employee/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.db.models.fields import CharField
+from django.utils import timezone
 # Create your models here.
+class Employee(models.Model):
+    
+    first_name=models.CharField(max_length=20)
+    last_name=models.CharField(max_length=20)
+    password=models.CharField(max_length=30)
+    email_id=models.CharField(max_length=30)
+    phone_no=models.IntegerField()
+    age=models.IntegerField()
+    aadhar_number=models.CharField(max_length=15)
+    sex=models.CharField(max_length=5)
+    address=models.CharField(max_length=100)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+
--- HMS/hotel_management_system/employee/views.py
+++ HMS/hotel_management_system/employee/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def employee_register(request):
+    #if request=="POST":
+        #first_name=
     return render(request, 'employee/employee_register.html', {})
